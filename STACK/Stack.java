cd94928144fbe59c9f1841249b2bed3159b2f15a
saqib1995 <36404532+saqib1995@users.noreply.github.com>
2019-01-07 19:41:05 -0800

    from git

--- STACK/Stack.java
+++ STACK/Stack.java
@@ -1,3 +1,42 @@
-public class Stack {
-csaefefefd
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stack<T> {
+    private int top = -1;
+    private final List<T> stack;
+
+    public Stack() {
+        stack = new ArrayList();
+    }
+
+    public boolean empty() {
+        if(top == -1) {
+            return true;
+        }
+        return false;
+    }
+
+    public void push(T value) {
+        top++;
+        stack.add(value);
+    }
+
+    public T pop() {
+        T answer;
+        if(empty()) {
+            return null;
+        }
+        answer = stack.get(top);
+        top--;
+        return answer;
+    }
+
+    public T peek() {
+        if(empty()) {
+            return null;
+        }
+        return stack.get(top);
+    }
+
+
 }
