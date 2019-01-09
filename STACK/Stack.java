5ee1bb5f82d6c2e6817b6a7b9711c1dbd7463469
saqib1995 <36404532+saqib1995@users.noreply.github.com>
2019-01-08 20:10:31 -0800

    from git

--- STACK/Stack.java
+++ STACK/Stack.java
@@ -2,41 +2,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Stack<T> {
-    private int top = -1;
     private final List<T> stack;
 
     public Stack() {
         stack = new ArrayList();
     }
 
-    public boolean empty() {
-        if(top == -1) {
-            return true;
-        }
-        return false;
-    }
-
     public void push(T value) {
-        top++;
         stack.add(value);
     }
 
     public T pop() {
         T answer;
-        if(empty()) {
+        if(stack.isEmpty()) {
             return null;
         }
-        answer = stack.get(top);
-        top--;
+        answer = stack.remove(stack.size() - 1);
         return answer;
     }
 
     public T peek() {
-        if(empty()) {
+        if(stack.isEmpty()) {
             return null;
         }
-        return stack.get(top);
+        return stack.get(stack.size() - 1);
     }
-
 
 }
